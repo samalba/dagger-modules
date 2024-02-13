a20ddadb8c9579f86ee14dfb1da8a98b473ba28d
Sam Alba <216487+samalba@users.noreply.github.com>
2024-02-12 18:21:51 -0800

    ssh: fixes
    Signed-off-by: Sam Alba <216487+samalba@users.noreply.github.com>

--- ssh/main.go
+++ ssh/main.go
@@ -17,28 +17,48 @@ type SshOpts struct {
 	Login        string
 }
 
-// example usage: "dagger call --destination machine.localdomain"
-func New(destination string, opts ...SshOpts) (*Ssh, error) {
+// FIXME: custom struct as args is currently unsupported
+// (Error: unsupported list of objects "SshOpts" for flag: opts)
+// Keeping the code for later
+//
+// // example usage: "dagger call --destination machine.localdomain"
+// func New(destination string, opts ...SshOpts) (*Ssh, error) {
+// 	baseCtr := dag.Container().From("alpine:3").WithExec([]string{"apk", "add", "--no-cache", "openssh-client"})
+// 	ssh := &Ssh{
+// 		Destination: destination,
+// 		Opts:        opts,
+// 		BaseCtr:     baseCtr,
+// 	}
+// 	return ssh, nil
+// }
+
+func New(destination string, identityFile *Secret) (*Ssh, error) {
 	baseCtr := dag.Container().From("alpine:3").WithExec([]string{"apk", "add", "--no-cache", "openssh-client"})
+
+	// FIXME: Currently only supporting few commands, see comments and FIXME above
+	opts := SshOpts{
+		IdentityFile: identityFile,
+	}
+
 	ssh := &Ssh{
 		Destination: destination,
-		Opts:        opts,
+		Opts:        []SshOpts{opts},
 		BaseCtr:     baseCtr,
 	}
 	return ssh, nil
 }
 
-// example usage: "dagger call --destination machine.localdomain command whoami
+// example usage: "dagger call --destination USER@HOST --identity-file file:${HOME}/.ssh/id_ed25519 command --args whoami stdout"
 func (m *Ssh) Command(args ...string) *Container {
 	ctr := m.BaseCtr
 
-	execArgs := []string{"/usr/bin/ssh"}
+	execArgs := []string{"/usr/bin/ssh", "-o", "StrictHostKeyChecking=no"}
 	for i, o := range m.Opts {
 		if o.IdentityFile != nil {
 			// this allows to support several keys if many opts were passed
-			keyPath := fmt.Sprintf("/key_{%d}", i)
-			ctr.WithMountedSecret(keyPath, o.IdentityFile)
-			args = append(execArgs, "-i", keyPath)
+			keyPath := fmt.Sprintf("/key_%d", i)
+			ctr = ctr.WithMountedSecret(keyPath, o.IdentityFile)
+			execArgs = append(execArgs, "-i", keyPath)
 		}
 		if o.Login != "" {
 			execArgs = append(execArgs, "-l", o.Login)
