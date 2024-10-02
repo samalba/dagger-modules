866a88757e67de9be1a9413b0e5058b8ad49001b
Sam Alba <216487+samalba@users.noreply.github.com>
2024-10-01 18:35:20 -0700

    smartmodule: removes unused code
    Signed-off-by: Sam Alba <216487+samalba@users.noreply.github.com>

--- smart-module/src/main/tools.py
+++ smart-module/src/main/tools.py
@@ -3,17 +3,6 @@ import dagger
 
 from pydantic import BaseModel
 
-# async def t_build_and_push(repository_url: str, branch: str, ref: str) -> str:
-#     """Builds and pushes a Docker image from a git repository to a registry.
-
-#     Args:
-#         repository_url: git repository URL name
-#         branch: branch to build from
-#         ref: full url of the remote image, including the registry
-#     """
-
-#     dir = dag.git(repository_url).branch(branch).tree()
-#     return await dag.container().build(context=dir).publish(address=ref)
 
 class Directory(BaseModel):
     d_directory_id: str
